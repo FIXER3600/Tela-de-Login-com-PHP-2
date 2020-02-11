3b962f5d546b7bcaa1a47bedca09897e476e338f
FIXER3600 <guilhermecomercial51@gmail.com>
2020-02-11 20:02:06 -0300

    first commit

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Pesquisar
+        </title>
+    </head>
+    <body>
+        <?php
+        $loginComSucesso = False;
+        $login = $_POST["nomeDoUsuario"];
+        $senha = $_POST["senhaDoUsuario"];
+    
+        $nomeValidoDeUsuario="aluno1";
+        $senhaValidaDeUsuario=123;
+
+        echo "<script> alert('".$login."')</script>";
+        echo "<script> alert('".$senha."')</script>";
+
+        if($login==$nomeValidoDeUsuario && $senha == $senhaValidaDeUsuario){$loginComSucesso = true;}
+    echo "alert('$loginComSucesso<br>')";
+    echo $loginComSucesso;
+    echo $loginComSucesso;
+
+    if($loginComSucesso==false){
+        echo "<script>alert('".$loginComSucesso."')</script>";
+        echo "<script>alert('$loginComSucesso era falso convertido como string vazia e login falhou')</script>";
+    }else{
+        echo "<script>alert('".$loginComSucesso."')</script>";
+        echo "<script>alert('Ou seja, Login com Sucesso')</script>";
+    }
+    ?>
+    </body>
+</html>
